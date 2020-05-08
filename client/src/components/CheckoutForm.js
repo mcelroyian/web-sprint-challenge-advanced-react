d66cc2c78506a0dd84dee27c9dcd92d7be359e90
Ian McElroy <mcelroyian@gmail.com>
2020-05-08 11:18:32 -0500

    basic test running

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -62,7 +62,7 @@ const CheckoutForm = (props) => {
           Zip:
           <input name="zip" value={values.zip} onChange={handleChanges} />
         </label>
-        <button>Checkout</button>
+        <button data-testid="checkout">Checkout</button>
       </form>
 
       {showSuccessMessage && (
--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,29 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import CheckoutForm from "./CheckoutForm";
+import React from "react"
+import { render, fireEvent, getByDisplayValue } from "@testing-library/react"
+import CheckoutForm from "./CheckoutForm"
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
+})
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    const { getByLabelText, getByText, getByDisplayValue, getByTestId} = render(<CheckoutForm />)
+
+    //grab inputs
+    const firstName = getByLabelText(/first name/i)
+    //checkinputs are there
+    expect(firstName).toBeInTheDocument()
+    //fill out inputs
+    fireEvent.change(firstName, { target: { value: 'Ian' } })
+    //checkthat values are showing in inputs
+    expect(getByDisplayValue(/ian/i)).toBeInTheDocument()
+    //grab submit button and click
+    const checkout = getByTestId('checkout')
+    fireEvent.click(checkout)
+    // Does message show?
+    expect(getByText(/you have ordered some plants/i)).toBeInTheDocument()
+    expect(getByText(/ian/i)).toBeInTheDocument()
+    
+})
